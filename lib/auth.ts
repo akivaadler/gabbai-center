99a331a1f14ad66fa86b01b2ee0ae54e2ff2acb4
akivaadler <akiva.adler@grip.security>
2026-04-06 10:54:48 +0300

    auto: update platform

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,6 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 import bcrypt from "bcryptjs";
 import { prisma } from "@/lib/prisma";
 
