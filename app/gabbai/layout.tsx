daa198fc0f95c067052546ddd6c289754aaaf730
akivaadler <akiva.adler@grip.security>
2026-04-13 14:40:40 +0300

    auto: update platform

--- app/gabbai/layout.tsx
+++ app/gabbai/layout.tsx
@@ -23,7 +23,7 @@ export default async function GabbaiLayout({
       {/* Main content area */}
       <div className="flex flex-1 flex-col overflow-hidden">
         <GabbaiHeader user={session.user} />
-        <main className="flex-1 overflow-y-auto p-6">
+        <main className="flex-1 overflow-y-auto p-4 md:p-6 pb-10">
           {children}
         </main>
       </div>
